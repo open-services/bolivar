f0085cdb9e2c4cad93f5a5b43a9347978791078e
victorbjelkholm <victorbjelkholm@gmail.com>
2019-05-15 17:12:04 +0200

    Implement simulator
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- simulator/project.clj
+++ simulator/project.clj
@@ -1,6 +1,8 @@
 (defproject simulator "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [lispyclouds/clj-docker-client "0.2.2"]]
+                 [lispyclouds/clj-docker-client "0.2.2"]
+                 [cheshire "5.8.1"]
+                 [oc-humanize "0.2.3-alpha1"]]
   :main ^:skip-aot simulator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- simulator/src/simulator/core.clj
+++ simulator/src/simulator/core.clj
@@ -1,102 +1,148 @@
 (ns simulator.core
-  (:require [clj-docker-client.core :as docker])
+  (:require [clj-docker-client.core :as docker]
+            [clojure.test :refer [is]]
+            [cheshire.core :refer [parse-string]]
+            [clojure.contrib.humanize :refer [filesize]])
   (:gen-class))
 
-(comment
+;; (comment
+;;   ;; start five registries with share off
+;;   ;; download packages
+;;   ;; start five registries with share on
+;;   ;; download packages
+;;   ;; second version is faster
+;; )
+;; 
+;; ;; faster as it's using sharing
+;; (def open-registry-id (run-open-registry))
+;; (def bolivar-ids (map (partial run bolivar) (range 5)))
+;; 
+;; ;; todo
+;; ;; - [ ] image for open-registry
+;; ;; - [ ] rename open-registry-fed -> bolivar
+;; ;; - [ ] methods for
+;; ;;  - [ ] running open-registry
+;; ;;  - [ ] running bolivar
+;; ;;  - [ ] running commands in existing container
 
-  ;; test cases
-  ;; start-registry
-  ;; download-packages
-  ;; download-packages
-  ;; second download packages is faster than first
+(def open-registry {:image "open-services/open-registry:latest"})
 
-  ;; start-registry
-  ;; start-registry
-  ;; download-packages from first
-  ;; download-packages from second
-  ;; second download packages is faster than first
+(def bolivar {:image "open-services/bolivar:latest"
+              :cmd "bolivar --http-address=0.0.0.0"})
 
-  ;; start five registries with share off
-  ;; download packages
-  ;; start five registries with share on
-  ;; download packages
-  ;; second version is faster
+(def deps-test {:image "open-services/deps-test:latest"})
 
-  (create-nodes 1 {:registry true})
+(def conn (docker/connect))
 
-  (defscenario cached-faster {:nodes 1 })
+(defn run [app]
+  (clojure.pprint/pprint app)
+  (docker/start conn (docker/create conn (:image app) (:cmd app) {} (:ports app))))
 
-)
-
-(def scenarios (atom []))
-
-(defmacro defscenario [title & forms])
-
-;; open-registry is the central index
-;; bolivar is a local federation proxy that runs a registry that can also share
-
-;; start open-registry (fresh index) (should have control over index?)
-;; start bolivar
-;; run test-project that downloads directly from open-registry
-;; run test-project that downloads via bolivar, keep running
-;; second test-project that downloads via bolivar
-
-;; todo
-;; - [ ] image for open-registry
-;; - [ ] rename open-registry-fed -> bolivar
-;; - [ ] methods for
-;;  - [ ] running open-registry
-;;  - [ ] running bolivar
-;;  - [ ] running commands in existing container
-
-(def test-net [:open-registry 1
-               :bolivar 2
-               :deps-test 2])
-
-(def meas1 (measure (install-deps (test-net :deps-test 1))))
-(def meas2 (measure (install-deps (test-net :deps-test 2))))
-(is (< meas1 meas1))
-
-;; dsl for testing and actions in that system
-
-;; model system for properties
-
-;; create container with node:12.0.0-stretch
-;; run open-registry binary in it
-;; git clone webpack
-;; rm yarn.lock
-;; set registry
-;; run yarn install and measure executing time
-
-(comment
-  (def conn (docker/connect))
-
-  (with-open [conn (docker/connect)]
-    (docker/ping conn))
-
-  ;; (docker/pull conn "node:12.0.0-stretch")
-
-  (def container-id (docker/create conn "open-registry-fed"))
-
-  (docker/start conn container-id)
-
+(defn exec [container-id cmd]
   (println container-id)
+  (println cmd))
 
-  (docker/logs conn container-id)
+;; Stolen from
+;; https://github.com/clojure/clojure/blob/clojure-1.9.0/src/clj/clojure/core.clj#L3850
+(defmacro measure
+  "Evaluates expr and return the time (in milliseconds) it took together with the result"
+  [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr
+         duration# (/ (double (- (. System (nanoTime)) start#)) 1000000.0)]
+     {:return ret#
+      :duration duration#}))
 
-  (docker/exec conn container-id "whoami")
-
-  (def container-ip  (-> (docker/inspect conn container-id)
-                              :NetworkSettings
-                              :Networks
-                              :bridge
-                              :IPAddress
-                              ))
-  "172.17.0.2"
-
+(comment
+  (measure (+ 1 1))
+  (measure (Thread/sleep 1000))
   )
 
+(defn run-tests [tests]
+  (doseq [t tests]
+    (let [ret (t)]
+      (clojure.pprint/pprint ret))))
+
+(defn get-ip [container-id]
+  (-> (docker/inspect conn container-id)
+      :NetworkSettings
+      :Networks
+      :bridge
+      :IPAddress
+      ))
+
+(defn run-with-registry [ip]
+  (format "bash -c \"yarn --verbose --registry=http://%s:8080\"" ip))
+
+(defn deps-test-image [ip]
+  {:image "open-services/deps-test:latest"
+   :cmd (run-with-registry ip)})
+
+(defn run-and-wait [conn image]
+  (let [id (run image)]
+    (docker/wait-container conn id)
+    id))
+
+(defn measure-install [conn bolivar-ip]
+  (measure (run-and-wait conn (deps-test-image bolivar-ip))))
+
+(defn bolivar-metrics [bolivar-ip]
+  (slurp (format "http://%s:8080/_api/metrics" bolivar-ip)))
+
+(defn clean [ids]
+  (doseq [id ids]
+    (try (docker/kill conn id)
+         (catch Exception e (println (str "didnt find container " id))))
+    (docker/rm conn id)))
+
+(defn get-metric [bolivar-ip k]
+  (k (parse-string (bolivar-metrics bolivar-ip) true)))
+
+(defn humanize-metrics [bolivar-ip]
+  (let [m(parse-string (bolivar-metrics bolivar-ip) true)]
+  {:TotalIn (filesize (:TotalIn m))
+   :TotalOut (filesize (:TotalOut m))
+   :RateIn (filesize (:RateIn m))
+   :RateOut (filesize (:RateOut m))}))
+
+;; (clojure.pprint/pprint (humanize-metrics bolivar-ip))
+
+;; test cases
+
+(defn second-install-cached []
+  (let [bolivar-id (run bolivar)
+        bolivar-ip (get-ip bolivar-id)]
+    (Thread/sleep 1000)
+    (let [m1 (measure-install conn bolivar-ip)
+          m2 (measure-install conn bolivar-ip)]
+      (is (< (:duration m2) (:duration m1)))
+      (clean [(:return m2) (:return m1)
+              bolivar-id]))))
+
+(defn second-bolivar-faster []
+  (let [bolivar-id (run bolivar)
+        bolivar-ip (get-ip bolivar-id)
+        bolivar2-id (run bolivar)
+        bolivar2-ip (get-ip bolivar2-id)]
+    (Thread/sleep 1000)
+    (let [m1 (measure-install conn bolivar-ip)
+          m2 (measure-install conn bolivar2-ip)]
+      (let [ti1 (get-metric bolivar-ip :TotalIn)
+            ti2 (get-metric bolivar2-ip :TotalIn)
+            to1 (get-metric bolivar-ip :TotalOut)
+            to2 (get-metric bolivar2-ip :TotalOut)]
+        (is (< (:duration m2) (:duration m1)))
+        (is (> to1 ti2))
+        (is (> to1 to2))
+        (is (> ti1 0))
+        (is (> ti2 0))
+        (is (> to1 0))
+        (is (> to2 0))
+        (clean [(:return m2) (:return m1)
+                bolivar-id bolivar2-id])))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-tests [second-install-cached
+              second-bolivar-faster])
+  (System/exit 0))
